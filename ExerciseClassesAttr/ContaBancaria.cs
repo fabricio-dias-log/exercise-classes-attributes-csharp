0f32a8cae747b5d99bb77f1f13ded064c1a6263d
Fabricio Dias <fabricio270909@gmail.com>
2024-06-08 22:37:13 -0300

    Exercise is done

---
+++ ExerciseClassesAttr/ContaBancaria.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+
+namespace ExerciseClassesAttr;
+
+public class ContaBancaria
+{
+    public int NumeroConta { get; private set; }
+    public string NomeTitular { get; set; }
+    public double Saldo { get; private set; } = 0;
+
+    public ContaBancaria()
+    {
+        
+    }
+
+    public ContaBancaria(int numeroConta, string nomeTitular, double depositoInicial) : this(numeroConta, nomeTitular)
+    {
+        Deposito(depositoInicial);
+    }
+
+    public ContaBancaria(int numeroConta, string nomeTitular)
+    {
+        NumeroConta = numeroConta;
+        NomeTitular = nomeTitular;
+    }
+
+    public void CadastrarConta()
+    {
+        
+        
+        Console.Clear();
+        Console.WriteLine("Dados da conta: ");
+        Console.WriteLine(this);
+    }
+
+    public void Deposito(double quantia)
+    {
+        this.Saldo += quantia;
+
+        Console.Clear();
+        Console.WriteLine("Dados atualizados: ");
+        Console.WriteLine(this);
+    }
+    
+    public void Saque(double quantia)
+    {
+        double taxaSaque = 5.00;
+        
+        this.Saldo -= quantia + taxaSaque;
+
+        Console.Clear();
+        Console.WriteLine("Dados atualizados: ");
+        Console.WriteLine(this);
+    }
+
+    public override string ToString()
+    {
+        return $"Conta: {this.NumeroConta} | Titular: {this.NomeTitular} | Saldo: R$ {this.Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+    }
+}
---
+++ ExerciseClassesAttr/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace ExerciseClassesAttr;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ContaBancaria contaBancaria;
+        int option;
+        double quantia;
+        char desejaValorInicial;
+        
+        Console.WriteLine("-----------------------------");
+        Console.WriteLine("Cadastro de Conta bancária:");
+        Console.WriteLine("-----------------------------");
+        Console.Write("Digite o número da conta: ");
+        int numeroConta = int.Parse(Console.ReadLine());
+        
+        Console.Write("Digite o nome do titular da conta: ");
+        string nomeTitular = Console.ReadLine();
+
+        Console.Write("Haverá valor inicial (s/n)?");
+        desejaValorInicial = char.Parse(Console.ReadLine());
+
+        if (desejaValorInicial == 's' || desejaValorInicial == 'S')
+        {
+            Console.Write("Digite o valor de depósito inicial: ");
+            double saldoInicial = double.Parse(Console.ReadLine());
+            contaBancaria = new ContaBancaria(numeroConta, nomeTitular, saldoInicial);
+            Console.Clear();
+            Console.WriteLine(contaBancaria);
+        }
+        else
+        {
+            contaBancaria = new ContaBancaria(numeroConta, nomeTitular);
+            Console.Clear();
+            Console.WriteLine(contaBancaria);
+        }
+        
+        do
+        {
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Digite a opção desejada:");
+            Console.WriteLine("1 - Dados da Conta bancária");
+            Console.WriteLine("2 - Depósito");
+            Console.WriteLine("3 - Saque");
+            Console.WriteLine("-----------------------------");
+            Console.Write("Opção: ");
+
+            option = int.Parse(Console.ReadLine());
+            
+            Console.Clear();
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine(contaBancaria);
+                    break;
+                case 2:
+                    Console.Write("Digite o valor para depósito: ");
+                    quantia = double.Parse(Console.ReadLine());
+                    
+                    contaBancaria.Deposito(quantia);
+                    break;
+                case 3:
+                    Console.Write("Digite o valor para saque: ");
+                    quantia = double.Parse(Console.ReadLine()) ;
+                    
+                    contaBancaria.Saque(quantia);
+                    break;
+                default:
+                    return;
+            }
+
+        } while (true);
+    }
+}
